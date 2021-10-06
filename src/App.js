c696566d4270f1c621ad8e26fac99049ab608f1e
Cynthia Nosiri <cynthiasamuels98@gmail.com>
2021-10-06 19:57:31 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,58 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import React, { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl'; 
+import neighbordata from './kc-neighborhoods.json';
+import tractdata from './kc-tracts.json';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY3lub3MxIiwiYSI6ImNrdWZveXQwcTF3ZTAyd3FscHV3c2JydWgifQ.EnstwudjuuiHNPsQWsRzag';
 
 
 
-// https://api.mapbox.com/datasets/v1/cynos1?access_token=pk.eyJ1IjoiY3lub3MxIiwiYSI6ImNrdWZveXQwcTF3ZTAyd3FscHV3c2JydWgifQ.EnstwudjuuiHNPsQWsRzag
+
 
 
 
 export default function App() {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
-    const [zoom, setZoom] = useState(9);
+    const mapContainer = useRef();
+    // const map = useRef(null);
+    // const [lng, setLng] = useState(-70.9);
+    // const [lat, setLat] = useState(42.35);
+    // const [zoom, setZoom] = useState(9);
+   
 
     useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
+        // if (map.current) return; // initialize map only once
+        const map = new mapboxgl.Map({
           container: mapContainer.current,
           style: 'mapbox://styles/mapbox/streets-v11',
-          center: [lng, lat],
-          zoom: zoom
+          center: [-70.9, 42.35],
+          zoom: 9
         });
-      });
+        
+
+
+        map.on("load", () =>{
+        //   map.addSource("neighbors", {
+        //     type: "vector",
+        //     data: neighbordata,
+            
+        // });
+
+        // map.addSource("tract", {
+        //   type: "geojson",
+        //   data: tractdata,
+        // });
+        map.addLayer({
+          
+        })
+        })
+         
+      return () => map.remove();  
+    },[]);
+
 
       return (
-        <div>
+        
           <div ref={mapContainer} className="map-container" />
-        </div>
+        
       );
 }
