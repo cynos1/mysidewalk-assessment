b261b06f88a5eaead0ae9673bbff1ff2ec1ca786
Cynthia Nosiri <cynthiasamuels98@gmail.com>
2021-10-10 21:31:41 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
 import React, { useRef, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl'; 
-import neighbordata from './kc-neighborhoods.json';
-import tractdata from './kc-tracts.json';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY3lub3MxIiwiYSI6ImNrdWZveXQwcTF3ZTAyd3FscHV3c2JydWgifQ.EnstwudjuuiHNPsQWsRzag';
 
-
-
-
-
-
-
 export default function App() {
     const mapContainer = useRef();
-    // const map = useRef(null);
-    // const [lng, setLng] = useState(-70.9);
-    // const [lat, setLat] = useState(42.35);
-    // const [zoom, setZoom] = useState(9);
-   
+ 
 
     useEffect(() => {
-        // if (map.current) return; // initialize map only once
+       
         const map = new mapboxgl.Map({
           container: mapContainer.current,
           style: 'mapbox://styles/mapbox/streets-v11',
-          center: [-70.9, 42.35],
+          center: [-94.4159888785519, 39.0263622595943],
           zoom: 9
         });
-        
-
 
         map.on("load", () =>{
-        //   map.addSource("neighbors", {
-        //     type: "vector",
-        //     data: neighbordata,
-            
-        // });
+          map.addSource("kc-neighborhoods", {
+            type: "geojson",
+            data: "kc-neighborhoods.json",
+          });
+          map.addSource("kc-tracts", {
+            type: "geojson",
+            data: "kc-tracts.json",
+          });
 
-        // map.addSource("tract", {
-        //   type: "geojson",
-        //   data: tractdata,
-        // });
-        map.addLayer({
-          
-        })
+          map.addLayer({
+            'id': 'kc-neighborhoods',
+            'type': 'fill',
+            'source': 'kc-neighborhoods',
+            'paint': {
+              'fill-color': '#0080ff',
+              'fill-opacity': 0.8
+            }
+          });
+
+          map.addLayer({
+            'id': 'kc-tracts',
+            'type': 'fill',
+            'source': 'kc-tracts',
+            'paint': {
+              'fill-color': 'yellow',
+              'fill-opacity': 0.3,
+              'fill-outline-color': 'white'
+            }
+          });
         })
          
       return () => map.remove();  
@@ -51,8 +53,8 @@ export default function App() {
 
 
       return (
-        
+
           <div ref={mapContainer} className="map-container" />
-        
+
       );
 }
